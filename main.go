f63531fec9d55f8a1d6656e5d04ddee6a10b1b99
michael.klug <michael.klug@bearingpoint.com>
2020-08-03 16:37:24 +0200

    init

--- main.go
+++ main.go
@@ -13,9 +13,13 @@ func main() {
 	h2 := func(w http.ResponseWriter, _ *http.Request) {
 		io.WriteString(w, "Hello from a HandleFunc #2!\n")
 	}
+	h3 := func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "Hello from a HandleFunc #3!\n")
+	}
 
 	http.HandleFunc("/", h1)
-	http.HandleFunc("/endpoint", h2)
+	http.HandleFunc("/h2", h2)
+	http.HandleFunc("/h3", h3)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
